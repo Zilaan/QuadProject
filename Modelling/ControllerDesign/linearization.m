2394f7bc529fad8462624d836a28b057d2f1f223
Mamadou <mamadou@student.chalmers.se>
2015-04-23 22:14:29 +0200

    added lqcontroller

---
+++ Modelling/ControllerDesign/linearization.m
@@ -0,0 +1,98 @@
+clc, clear
+
+syms Ix Iy Iz wx wy wz tx ty tz p r y
+syms pd rd yd 
+
+% r = phi
+% p = theta
+% y = psi
+
+beta = [r; p; y];
+betad = [rd; pd; yd];
+I = diag([Ix, Iy, Iz]);
+
+t = [tx; ty; tz];
+
+W = [wx; wy; wz];
+
+OmegaD = inv(I)*(-cross(W, I*W) + t);
+
+T = [1, 0, -sin(p);
+     0, cos(r), cos(p)*sin(r);
+     0, -sin(r), cos(p)*cos(r)];
+
+betad = inv(T)*W
+
+%%
+clc
+
+r0 = 0;
+p0 = 0;
+y0 = 0;
+wx0 = 0;
+wy0 = 0;
+wz0 = 0;
+
+tx0 = 0;
+ty0 = 0;
+tz0 = 0;
+
+f = [OmegaD; betad];
+x = [wx, wy, wz, r, p, y].';
+
+for k = 1:length(f) % A matrix
+    for n = 1:length(x)
+        
+        As(k, n) = diff(f(k), x(n));
+        
+    end
+end
+
+for k = 1:length(f) % A matrix
+    for n = 1:length(t)
+        
+        Bs(k, n) = diff(f(k), t(n));
+        
+    end
+end
+
+wx =0; 
+wy=0;  wz=0;  r=0;  p=0;  y=0; 
+Ix = 0.14e-6; 
+Iy= 0.14e-6; 
+Iz= 0.217e-6;
+A= eval(As);
+B= eval(Bs);
+C= [0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1];
+D= [ 0 0 0 ; 0 0 0 ; 0 0 0 ];
+clear As Bs  pd rd yd  Ix Iy Iz wx wy wz tx ty tz p r y beta betad Omega ...
+    OmegaD W  x y0 wx0 wy0 wz0 f I n p0 r0 k T tz0 tx0 ty0 t 
+
+
+
+%% Make discrete time state space model
+
+    sys = ss(A,B,C,D); % discrete time
+ 
+    
+    
+    
+%% 
+%LQR for the total system
+
+q = 0.1*[1,1,1,1,1,1];
+r= 1e9*[ 1 1 1];
+Qu=diag(r);
+Qx = diag(q);
+[K,S,E] = lqr(sys,Qx,Qu) ;
+K
+E
+Kr = -inv(sys.c*inv(sys.a-sys.b*K-eye(6))*sys.b) 
+
+    
+    
+    
+
+
+
+
