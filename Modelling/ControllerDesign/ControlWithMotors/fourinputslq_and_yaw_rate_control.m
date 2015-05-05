f43653751f7f3a396513bcebf4cedd709a72b929
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2015-05-05 15:50:06 +0200

    Code clean up

--- Modelling/ControllerDesign/ControlWithMotors/fourinputslq_and_yaw_rate_control.m
+++ Modelling/ControllerDesign/ControlWithMotors/fourinputslq_and_yaw_rate_control.m
@@ -1,53 +1,47 @@
-
-%%
-% %
-% LQ control with the four PWV  generated using state feedback
-
-
-
-
-
-%%
-
-% linerization
+%% LQ control with the four PWM generated using state feedback
 clc, clear
 
-syms Ix Iy Iz wx wy wz tx ty tz p r y w1 w2 w3 w4 t1 L k b
-syms pd rd yd 
 
-% r = phi
-% p = theta
-% y = psi
+% roll = phi    (x-axis, facing forward)
+% pitch = theta (y-axis, facing left)
+% yaw = psi     (z-axis, facing up)
 
-beta = [r; p; y];
-betad = [rd; pd; yd];
+% p = angular velocity around bodyframe x-axis
+% q = angular velocity around bodyframe y-axis
+% r = angular velocity around bodyframe z-axis
+
+% beta = [phi; theta; psi]
+% betad = [phiDot; thetaDot; psiDot]
+% omega = [p; q; r]
+% omegad = [pDot; qDot; rDot]
+
+% Linerization
+
+syms Ix Iy Iz phi theta psi tx ty tz p q r w1 w2 w3 w4 t1 L k b
+
 I = diag([Ix, Iy, Iz]);
 
-T_th= L*k*(w2+ w3-w1 -w4);
-T_ph=L*k*(w3+ w4-w1 -w2);
-T_ps= b*(w1-w2+w3-w4);
+T_th = L*k*(w2 + w3 - w1 - w4);
+T_ph = L*k*(w3 + w4 - w1 - w2);
+T_ps = b*(w1 - w2 + w3 - w4);
 
 
 t = [T_ph; T_th; T_ps];
-t1= [ w1; w2; w3; w4]; 
+w = [w1; w2; w3; w4]; 
 
-W = [wx; wy; wz];
+W = [p; q; r];
 
 OmegaD = inv(I)*(-cross(W, I*W) + t);
 
-T = [1, 0, -sin(p);
-     0, cos(r), cos(p)*sin(r);
-     0, -sin(r), cos(p)*cos(r)];
+T = [1, 0, -sin(theta);
+     0, cos(phi), cos(theta)*sin(phi);
+     0, -sin(phi), cos(theta)*cos(phi)];
 
 betad = inv(T)*W;
 
-%%
-
-
-
 
 f = [OmegaD; betad];
-x = [wx, wy, wz, r, p, y].';
+x = [p, q, r, phi, theta, psi].';
 
 for k = 1:length(f) % A matrix
     for n = 1:length(x)
@@ -57,88 +51,63 @@ for k = 1:length(f) % A matrix
     end
 end
 
-for k = 1:length(f) % A matrix
-    for n = 1:length(t1)
+for k = 1:length(f) % B matrix
+    for n = 1:length(w)
         
-        Bs(k, n) = diff(f(k), t1(n));
+        Bs(k, n) = diff(f(k), w(n));
         
     end
 end
 
-wx =0; 
-wy=0;  wz=0;  r=0;  p=0;  y=0; 
-Ix = 0.14e-6; 
-Iy= 0.14e-6; 
-Iz= 0.217e-6;
-L=  0.046;
-k= 2.75e-11;
-b=1e-9;
-A= eval(As);
-B= eval(Bs);
+p = 0; q = 0; r = 0;
+phi = 0; theta = 0; psi = 0;
+
+Ix = 0.14e-6;
+Iy = 0.14e-6;
+Iz = 0.217e-6;
+L =  0.046;
+k = 2.75e-11;
+b = 1e-9;
+A = eval(As);
+B = eval(Bs);
 % C= [0 0 1 0 0 0;0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1];
-C= [0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1];
+C = [0 0 0 1 0 0; 0 0 0 0 1 0; 0 0 0 0 0 1];
 % D= [ 0 0 0  ; 0 0 0 ; 0 0 0 ];
 clear As Bs w1 w2 w3 w4   pd rd yd  Ix Iy Iz wx wy wz tx ty tz p r y beta betad Omega ...
     OmegaD W  x y0 wx0 wy0 wz0 f I n p0 r0 k T tz0 tx0 ty0 t 
 
 
-
-
-%%
-%make state space
+%% Make state space
+% 
 sys = ss(A,B,C,0); 
 
+%% Check controllability
 
-
-    %%
-    %Check controllability
-    co = ctrb(sys);
+co = ctrb(sys);
 controllability = rank(co);
-    
-    
-    
-    
-    
-    
-    
-    
-    
-  %% 
-% %%LQR controller yaw rate, roll pitch 
-% %Ya rate controll 
-% remove all traces of the yaw to control yaw rate
 
- Aa=  A(:,1:5); 
- Aa=  Aa(1:5,:); 
-Bb= B(1:5,:);
+
+%% LQR controller yaw rate, roll pitch
+% Remove all traces of the yaw to control yaw rate
+
+Aa =  A(:,1:5);
+Aa =  Aa(1:5,:); 
+Bb = B(1:5,:);
 Cc = [0 0 1 0 0; 0 0 0 1 0; 0 0 0 0 1 ];
 
 
+phi = 1*[ 1 1 1 1];
+R = diag(phi);
+Q = (Cc'*Cc); 
+R = diag(phi);
+Q(3,3) = Q(3,3)*1e5;  % Yaw rate
+Q(4,4) = Q(4,4)*1e12;  % Roll
+Q(5,5) = Q(5,5)*1e12;  % Pitch
 
-r= 1*[ 1 1 1 1];
-R=diag(r);
-Q  = (Cc'*Cc); 
-R=diag(r);
-Q(3,3)= Q(3,3)*1e5;  %yaw rate
-Q(4,4)= Q(4,4)*1e12;  %roll
-Q(5,5)= Q(5,5)*1e12;  %pitch
-
-% selector matrix , to exclude the yaw from the closed loop system
- Cs = [1 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0;  0 0 0 1 0 0;0 0 0 0 1 0];
+% Selector matrix, to exclude the yaw from the closed loop system
+Cs = [1 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0;  0 0 0 1 0 0;0 0 0 0 1 0];
 
 [K,Ss,Ee] = lqr(Aa,Bb,Q,R) ;
     
-Kr = -inv(Cc*inv(Aa-Bb*K)*Bb(:,2:4))  ; %refernce traking for yaw rate, roll and pitch 
-    
-    
-    
-    
-    
- 
-   
-    
-    
-  
+Kr = -inv(Cc*inv(Aa-Bb*K)*Bb(:,2:4)); % Reference traking for yaw rate, roll and pitch 
 
-
-    
--- Modelling/ControllerDesign/ControlWithTorques/Three_torque_input.m
+++ Modelling/ControllerDesign/ControlWithTorques/Three_torque_input.m
@@ -1,44 +1,41 @@
-% LQ control with the three control torque generated using state feedback
-
-
-% Linearization
+%% LQ control with the three control torque generated using state feedback
 
 clc, clear
 
+% roll = phi    (x-axis, facing forward)
+% pitch = theta (y-axis, facing left)
+% yaw = psi     (z-axis, facing up)
 
-syms Ix Iy Iz wx wy wz tx ty tz p r y w1 w2 w3 w4 t1 L k b T_ph T_th T_ps
-syms pd rd yd 
+% p = angular velocity around bodyframe x-axis
+% q = angular velocity around bodyframe y-axis
+% r = angular velocity around bodyframe z-axis
 
-% r = phi
-% p = theta
-% y = psi
+% beta = [phi; theta; psi]
+% betad = [phiDot; thetaDot; psiDot]
+% omega = [p; q; r]
+% omegad = [pDot; qDot; rDot]
 
-beta = [r; p; y];
-betad = [rd; pd; yd];
+% Linearization
+
+syms Ix Iy Iz tx ty tz p q r w1 w2 w3 w4 t1 L k b T_ph T_th T_ps
+syms phi theta psi
+
 I = diag([Ix, Iy, Iz]);
-
-
 
 t = [T_ph; T_th; T_ps];
 
-
-W = [wx; wy; wz];
+W = [p; q; r];
 
 OmegaD = inv(I)*(-cross(W, I*W) + t);
 
-T = [1, 0, -sin(p);
-     0, cos(r), cos(p)*sin(r);
-     0, -sin(r), cos(p)*cos(r)];
+T = [1, 0, -sin(theta);
+     0, cos(phi), cos(theta)*sin(phi);
+     0, -sin(phi), cos(theta)*cos(phi)];
 
 betad = inv(T)*W;
 
-%%
-
-
-
-
 f = [OmegaD; betad];
-x = [wx, wy, wz, r, p, y].';
+x = [p, q, r, phi, theta, psi].';
 
 for k = 1:length(f) % A matrix
     for n = 1:length(x)
@@ -48,7 +45,7 @@ for k = 1:length(f) % A matrix
     end
 end
 
-for k = 1:length(f) % A matrix
+for k = 1:length(f) % B matrix
     for n = 1:length(t)
         
         Bs(k, n) = diff(f(k), t(n));
@@ -56,59 +53,50 @@ for k = 1:length(f) % A matrix
     end
 end
 
-%% 
-%Numerique evaluation using the known data
-wx =0; 
-wy=0;  wz=0;  r=0;  p=0;  y=0; 
+%% Numerique evaluation using the known data
+
+p = 0; q = 0; r = 0;
+phi = 0; theta = 0; psi = 0; 
+
 Ix = 0.14e-6; 
-Iy= 0.14e-6; 
-Iz= 0.217e-6;
-L=  0.046;
-k= 2.75e-11;
-b=1e-9;
-A= eval(As);
-B= eval(Bs);
-C= eye(6); 
-D=0; 
+Iy = 0.14e-6; 
+Iz = 0.217e-6;
+L = 0.046;
+k = 2.75e-11;
+b = 1e-9;
+A =  eval(As);
+B = eval(Bs);
+C = eye(6); 
+D = 0; 
 clear As Bs w1 w2 w3 w4   pd rd yd  Ix Iy Iz wx wy wz tx ty tz p r y beta betad Omega ...
     OmegaD W  x y0 wx0 wy0 wz0 f I n p0 r0 k T tz0 tx0 ty0 t 
 
 
+%% Make state space
 
-
-%%
-%make state space
 sys = ss(A,B,C,0); 
 
+%% Check controllability
 
-
- %%
-  %Check controllability
-    co = ctrb(sys);
+co = ctrb(sys);
 controllability = rank(co)
 
-
-
-%%
-% LQR controller yaw rate, roll pitch 
-% Yaw rate controll 
+%% LQR controller yaw rate, roll pitch 
 % We must first remove any trace of the yaw to be able to control yaw rate
 % .... so we get a new system
-Aa=  A(:,1:5);
-Aa=  Aa(1:5,:);
-Bb= B(1:5,:);
+
+Aa = A(:,1:5);
+Aa = Aa(1:5,:);
+Bb = B(1:5,:);
 Cc = [0 0 1 0 0; 0 0 0 1 0; 0 0 0 0 1 ]; %outputs to be used in the Reference tracking
 
-
-%%
-
 % Weights
-r= 1e12*[ 1 1 1];
-R=diag(r);
+phi = 1e12*[1 1 1];
+R = diag(phi);
 Q = (Cc'*Cc); 
-Q(3,3)= Q(3,3);  %  yaw rate weight
-Q(4,4)= Q(4,4); %  roll weight
-Q(5,5)= Q(5,5); %  pitch weight
+Q(3,3) = Q(3,3);  %  yaw rate weight
+Q(4,4) = Q(4,4); %  roll weight
+Q(5,5) = Q(5,5); %  pitch weight
 
 % Selector matrix to feedback only the 5 states without the yaw from the
 % complete model
@@ -117,7 +105,4 @@ Cs = [1 0 0 0 0 0; 0 1 0 0 0 0; 0 0 1 0 0 0;  0 0 0 1 0 0;0 0 0 0 1 0];
 [K,S,E] = lqr(Aa,Bb,Q,R) ;
     
 Kr = -inv(Cc*inv(Aa-Bb*K)*Bb) ; 
-
-
-
 
